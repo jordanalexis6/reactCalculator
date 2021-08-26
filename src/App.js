a5cbbc1b8397d870006e043d77fb30e953c174e5
jordanalexis6 <jordan.a.neill@gmail.com>
2021-08-26 18:23:51 -0500

    adding button labels

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ const App = () => {
           <input type="text" value={result} />
         </form>
         <div className="keypad">
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
+          <button>Clear</button>
+          <button>C</button>
+          <button>&divide;</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>&times;</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>&ndash;</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
         </div>
       </div>
     </>
